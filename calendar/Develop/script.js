ba5698345506286e51248910c19e71eb4ddbc409
treesaretall <himatthewhurst@hotmail.com>
2023-05-16 10:30:12 -0500

    Added button and local storage functions and cleaned up repetitive code

--- calendar/Develop/script.js
+++ calendar/Develop/script.js
@@ -1,108 +1,73 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 const currentTime = dayjs().format('HH:mm:ss');
-let currentDay = $('#currentDay');
-let nine = $('#hour-9');
-let ten = $('#hour-10');
-let eleven = $('#hour-11');
-let twelve = $('#hour-12');
-let one = $('#hour-1');
-let two = $('#hour-2');
-let three = $('#hour-3');
-let four = $('#hour-4');
-let five = $('#hour-5');
+const currentDay = $('#currentDay');
+const nine = $('#hour-9');
+const ten = $('#hour-10');
+const eleven = $('#hour-11');
+const twelve = $('#hour-12');
+const one = $('#hour-13');
+const two = $('#hour-14');
+const three = $('#hour-15');
+const four = $('#hour-16');
+const five = $('#hour-17');
 
 // Function to update the clock display
 function updateClock() {
   currentDay.text(currentTime);
 };
 updateClock();
-setInterval(updateClock, 1000);
 
+
+// Compare the current time with the target time and assign colors
 $(document).ready(function() {
-  var targetTime = new Date();
+  let targetTime = new Date();
+  let elements = [
+    { element: nine, hour: 9 },
+    { element: ten, hour: 10 },
+    { element: eleven, hour: 11 },
+    { element: twelve, hour: 12 },
+    { element: one, hour: 13 },
+    { element: two, hour: 14 },
+    { element: three, hour: 15 },
+    { element: four, hour: 16 },
+    { element: five, hour: 17 }
+  ];
+  elements.forEach(function(item) {
+    let element = item.element;
+    let hour = item.hour;
+    if (dayjs().isBefore(targetTime.setHours(hour), 'hour')) {
+      element.removeClass('past present').addClass('future');
+    } else if (dayjs().isSame(targetTime.setHours(hour), 'hour')) {
+      element.removeClass('past future').addClass('present');
+    } else if (dayjs().isAfter(targetTime.setHours(hour), 'hour')) {
+      element.removeClass('present future').addClass('past');
+    };
+    targetTime = new Date();
+  });
+});
 
-  // Compare the current time with the target time
-  if (dayjs().isBefore(targetTime.setHours(9), 'hour')) {
-    nine.removeClass('past present').addClass('future');
-  } else if (dayjs().isSame(targetTime.setHours(9), 'hour')) {
-    nine.removeClass('past future').addClass('present');
-  } else if (dayjs().isAfter(targetTime.setHours(9), 'hour')) {
-    nine.removeClass('present future').addClass('past');
-  }
+//Assigns event listeners to each button and saves input to local storage.
+for (let i = 9; i <= 17; i++) {
+  // Get the button and textarea elements based on their IDs
+  let button = $(`#hour-${i} button`);
+  let textarea = $(`#hour-${i} textarea`);
 
-  if (dayjs().isBefore(targetTime.setHours(10), 'hour')) {
-    ten.removeClass('past present').addClass('future');
-  } else if (dayjs().isSame(targetTime.setHours(10), 'hour')) {
-    ten.removeClass('past future').addClass('present');
-  } else if (dayjs().isAfter(targetTime.setHours(10), 'hour')) {
-    ten.removeClass('present future').addClass('past');
-  }
-  if (dayjs().isBefore(targetTime.setHours(11), 'hour')) {
-    eleven.removeClass('past present').addClass('future');
-  } else if (dayjs().isSame(targetTime.setHours(11), 'hour')) {
-    eleven.removeClass('past future').addClass('present');
-  } else if (dayjs().isAfter(targetTime.setHours(11), 'hour')) {
-    eleven.removeClass('present future').addClass('past');
-  }
-  if (dayjs().isBefore(targetTime.setHours(12), 'hour')) {
-    twelve.removeClass('past present').addClass('future');
-  } else if (dayjs().isSame(targetTime.setHours(12), 'hour')) {
-    twelve.removeClass('past future').addClass('present');
-  } else if (dayjs().isAfter(targetTime.setHours(12), 'hour')) {
-    twelve.removeClass('present future').addClass('past');
-  }
-  if (dayjs().isBefore(targetTime.setHours(13), 'hour')) {
-    one.removeClass('past present').addClass('future');
-  } else if (dayjs().isSame(targetTime.setHours(13), 'hour')) {
-    one.removeClass('past future').addClass('present');
-  } else if (dayjs().isAfter(targetTime.setHours(13), 'hour')) {
-    one.removeClass('present future').addClass('past');
-  }
-  if (dayjs().isBefore(targetTime.setHours(14), 'hour')) {
-    two.removeClass('past present').addClass('future');
-  } else if (dayjs().isSame(targetTime.setHours(14), 'hour')) {
-    two.removeClass('past future').addClass('present');
-  } else if (dayjs().isAfter(targetTime.setHours(14), 'hour')) {
-    two.removeClass('present future').addClass('past');
-  }
-  if (dayjs().isBefore(targetTime.setHours(15), 'hour')) {
-    three.removeClass('past present').addClass('future');
-  } else if (dayjs().isSame(targetTime.setHours(15), 'hour')) {
-    three.removeClass('past future').addClass('present');
-  } else if (dayjs().isAfter(targetTime.setHours(15), 'hour')) {
-    three.removeClass('present future').addClass('past');
-  }
-  if (dayjs().isBefore(targetTime.setHours(16), 'hour')) {
-    four.removeClass('past present').addClass('future');
-  } else if (dayjs().isSame(targetTime.setHours(16), 'hour')) {
-    four.removeClass('past future').addClass('present');
-  } else if (dayjs().isAfter(targetTime.setHours(16), 'hour')) {
-    four.removeClass('present future').addClass('past');
-  }
-  if (dayjs().isBefore(targetTime.setHours(17), 'hour')) {
-    five.removeClass('past present').addClass('future');
-  } else if (dayjs().isSame(targetTime.setHours(17), 'hour')) {
-    five.removeClass('past future').addClass('present');
-  } else if (dayjs().isAfter(targetTime.setHours(17), 'hour')) {
-    five.removeClass('present future').addClass('past');
-  }
-})
+  // Attach event listener to the button
+  button.on('click', function(event) {
+    let text = textarea.val();
+    localStorage.setItem(`savedText${i}`, text);
+    alert('Saved!');
+  });
+}
 
+//Get text from local storage and print it in calendar
+for (let i = 9; i <= 17; i++) {
+  let textarea = $(`#hour-${i} textarea`);
+  (function() {
+    let savedCont = localStorage.getItem(`savedText${i}`);
+    textarea.text(savedCont);
+  })();
+}
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
